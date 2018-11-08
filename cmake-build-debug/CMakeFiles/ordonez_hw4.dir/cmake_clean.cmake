20f7aefe901004530830c6b89ea34e8e01c928f2
galvane <lgalvanevelyn@gmail.com>
2018-11-08 01:31:31 -0500

    lexer

---
+++ cmake-build-debug/CMakeFiles/ordonez_hw4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ordonez_hw4.dir/main.cpp.o"
+  "ordonez_hw4.pdb"
+  "ordonez_hw4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ordonez_hw4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
