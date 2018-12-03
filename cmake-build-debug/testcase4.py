706bd59dc29c3560ddc8b6cfec41ac8787b0dfe2
galvane <lgalvanevelyn@gmail.com>
2018-12-03 14:20:32 -0500

    added hw2's testcase 4

--- cmake-build-debug/testcase4.py
+++ cmake-build-debug/testcase4.py
@@ -1,10 +1,10 @@
 # A function program
 def f():
-    a = 100
-    b = 200
-    c = 300
-    m = a * b + c
-    return m
+   a = 100
+   b = 200
+   c = 300
+   m = a * b + c
+   return m
 
 x = f()
 print("value of function is=",x)
